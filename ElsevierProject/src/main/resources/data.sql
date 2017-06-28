488c1b33bb10777375a861260d42e20a45ee6bed
Bui <BUIJ@science.regn.net>
2017-06-28 14:33:54 -0400

    added more dummy data and also all books page displays books yo

--- ElsevierProject/src/main/resources/data.sql
+++ ElsevierProject/src/main/resources/data.sql
@@ -7,14 +7,37 @@ DELETE FROM customer;
 INSERT INTO `book` (`book_id`, `title`, `book_image`, `format`, `price`, `isbn`, `description`, `page_count`, `publisher`, `published_date`, `table_of_contents`) VALUES ('0', 'Things Fall Apart', 'images/ThingsFallApart.jpg', 'Paperback', '3.50', '385474547', 'Things Fall Apart tells two intertwining stories, both centering no Okonkwo, a "strong man" of an Ibo village in Nigeria. The first, a powerful fable of the immemorial conflict between the individual and society, traces Okonkwos fall from grace with the tribal world. The second as modern as the first is ancient, concerns the clash of cultures and the destruction of Okonkwos world with the arrival of aggressive European missionaries. \n these perfectly harmonized twin dramas are informed by an awareness capable of encompassing at once the life of nature, human history, and the mysterious compulsions of the soul.', '205', 'William Heinemann Ltd.', '1958', '');
 INSERT INTO `book` (`book_id`, `title`, `book_image`, `format`, `price`, `isbn`, `description`, `page_count`, `publisher`, `published_date`, `table_of_contents`) VALUES ('1', 'The Complete Hans Christian Andersen Fairy Tales', 'images/fairytales.jpg', 'Hardcover', '1.00', '517092913', 'Fairy Tales!!!', '816', 'Gramercy', '1993' , '');
 INSERT INTO `book` (`book_id`, `title`, `book_image`, `format`, `price`, `isbn`, `description`, `page_count`, `publisher`, `published_date`, `table_of_contents`) VALUES ('2', 'The Divine Comedy (The Inferno, The Purgatorio, and The Paradiso)', 'images/divinecomedy.jpg', 'Paperback', '11.00', '451208633', 'HELL!!', '928', 'Berkley', '2003', '');
+INSERT INTO `book` (`book_id`, `title`, `book_image`, `format`, `price`, `isbn`, `description`, `page_count`, `publisher`, `published_date`, `table_of_contents`) VALUES ('3', 'Anna Karenina', 'images/divinecomedy.jpg', 'Paperback', '4.00', '24352345', 'Love affairs are bad', '928', 'Berkley', '2003', '');
+INSERT INTO `book` (`book_id`, `title`, `book_image`, `format`, `price`, `isbn`, `description`, `page_count`, `publisher`, `published_date`, `table_of_contents`) VALUES ('4', 'Madame Bovary', 'images/divinecomedy.jpg', 'Paperback', '7.00', '23452345', 'never listen to your heart. bad idea.', '928', 'Berkley', '2004', '');
+INSERT INTO `book` (`book_id`, `title`, `book_image`, `format`, `price`, `isbn`, `description`, `page_count`, `publisher`, `published_date`, `table_of_contents`) VALUES ('5', 'War and Peace', 'images/divinecomedy.jpg', 'Paperback', '12.00', '12341243', 'There are a lot of characters with a lot of history and they are all connected and great times. also napolean. ', '928', 'Berkley', '2004', '');
+INSERT INTO `book` (`book_id`, `title`, `book_image`, `format`, `price`, `isbn`, `description`, `page_count`, `publisher`, `published_date`, `table_of_contents`) VALUES ('6', 'The Great Gatsby', 'images/divinecomedy.jpg', 'Paperback', '5.00', '4523452', 'Jazz age. something something american dream. old money < new money', '928', 'Berkley', '2004', '');
+INSERT INTO `book` (`book_id`, `title`, `book_image`, `format`, `price`, `isbn`, `description`, `page_count`, `publisher`, `published_date`, `table_of_contents`) VALUES ('7', 'Lolita', 'images/divinecomedy.jpg', 'Paperback', '11.00', '4523452', 'controversial and gross', '928', 'Berkley', '1947', '');
+INSERT INTO `book` (`book_id`, `title`, `book_image`, `format`, `price`, `isbn`, `description`, `page_count`, `publisher`, `published_date`, `table_of_contents`) VALUES ('8', 'Middlemarch', 'images/divinecomedy.jpg', 'Paperback', '11.00', '4523452', 'a study of povincial life', '928', 'Berkley', '1947', '');
+INSERT INTO `book` (`book_id`, `title`, `book_image`, `format`, `price`, `isbn`, `description`, `page_count`, `publisher`, `published_date`, `table_of_contents`) VALUES ('9', 'The Adventures of Huckleberry Finn', 'images/divinecomedy.jpg', 'Paperback', '3.75', '4523452', 'boy oblivious of his dreadful life goes on great adventures', '928', 'Berkley', '1947', '');
+
 
 INSERT INTO `author` (`author_id`, `about_author`, `affiliations`, `author_name`, `expertise`) VALUES ('1', 'Fairy Tales', NULL, 'Hans Christian Andersen', 'Fairy Tales');
 INSERT INTO `author` (`author_id`, `about_author`, `affiliations`, `author_name`, `expertise`) VALUES ('0', 'YAMS', NULL, 'Chinua Achebe', 'YAMS');
 INSERT INTO `author` (`author_id`, `about_author`, `affiliations`, `author_name`, `expertise`) VALUES ('2', 'Inferno', NULL, 'Dante Alighieri', 'Purgatorio');
+INSERT INTO `author` (`author_id`, `about_author`, `affiliations`, `author_name`, `expertise`) VALUES ('3', 'Anne Karenina', NULL, 'Leo Tolstoy', 'Anne Karenina');
+INSERT INTO `author` (`author_id`, `about_author`, `affiliations`, `author_name`, `expertise`) VALUES ('4', 'Madame Bovary', NULL, 'Gustave', 'Madame Bovary');
+INSERT INTO `author` (`author_id`, `about_author`, `affiliations`, `author_name`, `expertise`) VALUES ('5', 'Gatsby', NULL, 'F. Scott Fitzgerald', 'the twenties!');
+INSERT INTO `author` (`author_id`, `about_author`, `affiliations`, `author_name`, `expertise`) VALUES ('6', 'Lolita', NULL, 'Vladimir Nabokov', 'internationally famous');
+INSERT INTO `author` (`author_id`, `about_author`, `affiliations`, `author_name`, `expertise`) VALUES ('7', 'Middlemarch', NULL, 'George Eliot', 'pen name for Mary Anne Evans');
+INSERT INTO `author` (`author_id`, `about_author`, `affiliations`, `author_name`, `expertise`) VALUES ('8', 'Huck Finn', NULL, 'Mark Twain', 'Americans love him');
 
 INSERT INTO `book_authors` (`book_book_id`, `authors_author_id`) VALUES ('0', '0');
 INSERT INTO `book_authors` (`book_book_id`, `authors_author_id`) VALUES ('1', '1');
 INSERT INTO `book_authors` (`book_book_id`, `authors_author_id`) VALUES ('2', '2');
+INSERT INTO `book_authors` (`book_book_id`, `authors_author_id`) VALUES ('3', '3');
+INSERT INTO `book_authors` (`book_book_id`, `authors_author_id`) VALUES ('4', '4');
+INSERT INTO `book_authors` (`book_book_id`, `authors_author_id`) VALUES ('5', '3');
+INSERT INTO `book_authors` (`book_book_id`, `authors_author_id`) VALUES ('6', '5');
+INSERT INTO `book_authors` (`book_book_id`, `authors_author_id`) VALUES ('7', '6');
+INSERT INTO `book_authors` (`book_book_id`, `authors_author_id`) VALUES ('8', '7');
+INSERT INTO `book_authors` (`book_book_id`, `authors_author_id`) VALUES ('9', '8');
+
+
 
 INSERT INTO `customer` (`customer_id`, `first_name`, `last_name`, `email`, `password`) VALUES ('0',  'Bob', 'Doe', 'bobdoe@email.com', 'password');
 INSERT INTO `customer` (`customer_id`, `first_name`, `last_name`, `email`, `password`) VALUES ('0',  'John', 'Doe', 'johndoe@email.com', 'password');
