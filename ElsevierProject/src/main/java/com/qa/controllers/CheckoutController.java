bac06da374da5fa1d9316aafd4daa9dcd34ece86
Danielfinigan <danielfinigan@gmail.com>
2017-06-28 19:11:53 +0000

    Removed unused code segments

--- ElsevierProject/src/main/java/com/qa/controllers/CheckoutController.java
+++ ElsevierProject/src/main/java/com/qa/controllers/CheckoutController.java
@@ -1,20 +1,15 @@
 package com.qa.controllers;
 
-import java.util.Map;
-
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.qa.models.Address;
 import com.qa.models.Customer;
-import com.qa.repositories.AddressRepository;
 
 @SessionAttributes(names={"book_counts"})
 @Controller
@@ -25,11 +20,9 @@ public class CheckoutController {
 	{	
 		ModelAndView modelAndView = new ModelAndView("billing_address");
 		shipping.setAddressType("shipping");
-		shipping.setCustomerId(customer.getCustomerId());
-		modelAndView.addObject("shipping_address", shipping);
-		System.out.println("\nShipping: " + shipping);
-		
+		shipping.setCustomerId(customer.getCustomerId());		
 		session.setAttribute("shipping_address", shipping);
+		
 	    return modelAndView;
 	}
 	
@@ -38,11 +31,8 @@ public class CheckoutController {
 	{
 		ModelAndView modelAndView = new ModelAndView("payment_form");
 		billing.setAddressType("billing");
-//		modelAndView.addObject("shipping_address", shipping);
 		billing.setCustomerId(customer.getCustomerId());
 		session.setAttribute("billing_address", billing);
-//		modelAndView.addObject("billing_address", billing);
-//		System.out.println("\nShipping: " + shipping);
 
 	    return modelAndView;
 	}
@@ -50,6 +40,7 @@ public class CheckoutController {
 	@RequestMapping("/paymentProcess")
 	public ModelAndView paymentProcess(){
 		ModelAndView modelAndView = new ModelAndView("confirm_order");
+		
 	    return modelAndView;
 	}
 	
@@ -57,15 +48,9 @@ public class CheckoutController {
 	public ModelAndView confirmOrder() {
 		ModelAndView modelAndView = new ModelAndView("index");
 		modelAndView.addObject("order_recieved", true);
+		
 		return modelAndView;
 	}
-	
-//	@RequestMapping("/submitBilling")
-//	public ModelAndView submitBilling(){
-//		ModelAndView modelAndView = new ModelAndView("payment_form");
-//		modelAndView.addObject("same", true);
-//		return modelAndView;
-//	}
 
 	
 	
